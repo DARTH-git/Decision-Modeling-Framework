cf3c7a25073728558bf9dd093b1665e973cd612d
Fernando Alarid-Escudero <alari006@umn.edu>
2019-02-21 00:36:04 -0600

    Update framework documentation

--- functions/05b_probabilistic-analysis_functions.R
+++ functions/05b_probabilistic-analysis_functions.R
@@ -1,8 +1,18 @@
 #---------------------------------------------------#
 #### Generate PSA dataset of CEA parameters ####
 #---------------------------------------------------#
-f.generate_psa_params <- function(seed = 7783, n.sim){ # User defined
-  # Load calibrated parameters
+f.generate_psa_params <- function(n.sim, seed = 20190220){ # User defined
+  ### Definition:
+  ##  Generates PSA input dataset by sampling decision model parameters from
+  ##  their distributions
+  ### Arguments:  
+  ##   n.sim: Number of PSA samples
+  ##   seed:  Seed for reproducibility of Monte Carlo sampling
+  ### Returns:
+  ##   df.psa.params: Dataframe with samples of parameters for PSA
+  ##
+  
+  ## Load calibrated parameters
   load("data/03_imis-output.rData")
   n.sim <- nrow(m.calib.post)
   set.seed <- seed
@@ -13,18 +23,18 @@ f.generate_psa_params <- function(seed = 7783, n.sim){ # User defined
     # hr.S1  = v.calib.post.map["hr.S1"], # hazard ratio of death in S1 vs healthy
     # hr.S2  = v.calib.post.map["hr.S2"], # hazard ratio of death in S2 vs healthy
     
-    ## Transition probabilities (per cycle)
+    ### Transition probabilities (per cycle)
     p.HS1   = rbeta(n.sim, 30, 170),        # probability to become sick when healthy
     p.S1H   = rbeta(n.sim, 60, 60) ,        # probability to become healthy when sick
     
-    ## State rewards
-    # Costs
+    ### State rewards
+    ## Costs
     c.H   = rgamma(n.sim, shape = 100, scale = 20)    , # cost of remaining one cycle in state H
     c.S1  = rgamma(n.sim, shape = 177.8, scale = 22.5), # cost of remaining one cycle in state S1
     c.S2  = rgamma(n.sim, shape = 225, scale = 66.7)  , # cost of remaining one cycle in state S2
     c.Trt = rgamma(n.sim, shape = 73.5, scale = 163.3), # cost of treatment (per cycle)
     c.D   = 0                                         , # cost of being in the death state
-    # Utilities
+    ## Utilities
     u.H   = rtruncnorm(n.sim, mean =    1, sd = 0.01, b = 1), # utility when healthy
     u.S1  = rtruncnorm(n.sim, mean = 0.75, sd = 0.02, b = 1), # utility when sick
     u.S2  = rtruncnorm(n.sim, mean = 0.50, sd = 0.03, b = 1), # utility when sicker
