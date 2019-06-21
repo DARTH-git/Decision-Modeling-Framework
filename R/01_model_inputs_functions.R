2a149559caa24013a836e4c46f7b892a2b112396
Alan <alanyny.yang@mail.utoronto.ca>
2019-06-21 17:25:12 -0400

    still changing naming convention

--- R/01_model_inputs_functions.R
+++ R/01_model_inputs_functions.R
@@ -8,19 +8,19 @@
 ### Returns:
 ##   v.r.mort_by_age: Vector with mortality by age
 ##
-f.load_mort_data <- function(file = "data/01_all-cause-mortality.csv"){
-  df.r.mort_by_age <- read.csv(file = file)
-  v.r.mort_by_age  <- df.r.mort_by_age %>%
+load_mort_data <- function(file = "data/01_all_cause_mortality.csv"){
+  df_r_mort_by_age <- read.csv(file = file)
+  v_r_mort_by_age  <- df_r_mort_by_age %>%
     dplyr::select(Total) %>%
     as.matrix()              # vector with mortality rates
-  return(v.r.mort_by_age)
+  return(v_r_mort_by_age)
 }
 
 #-----------------------------------------#
 #### Load all parameters               ####
 #-----------------------------------------#
-f.load_all_params <- function(file.init = "data/01_init-params.csv",
-                              file.mort = "data/01_all-cause-mortality.csv"){ # User defined
+load_all_params <- function(file.init = "data/01_init_params.csv",
+                            file.mort = "data/01_all_cause_mortality.csv"){ # User defined
   ### Definition:
   ##   Loads all parameters for the decision model from multiple sources and 
   ##   creates a list
@@ -30,36 +30,36 @@ f.load_all_params <- function(file.init = "data/01_init-params.csv",
   ##   file.mort: String with the location and name of the file with mortality 
   ##              data
   ### Returns:
-  ##   l.all.params: List with all parameters used for the decision model
+  ##   l_all_params: List with all parameters used for the decision model
   ##
   
   #### Load initial set of initial parameters form .csv file ####
-  df.params.init  <- read.csv(file = file.init)
+  df_params_init  <- read.csv(file = file.init)
   
   #### All-cause age-specific mortality from .csv file ####
-  v.r.mort_by_age <- f.load_mort_data(file = file.mort)
+  v_r_mort_by_age <- load_mort_data(file = file.mort)
   
-  l.params.all <- with(as.list(df.params.init), {
+  l_params_all <- with(as.list(df_params_init), {
     #### General setup ####
-    v.age.names <- n.age.init:(n.age.init + n.t - 1) # vector with age names
-    v.n <- c("H", "S1", "S2", "D")  # vector with the 4 health states of the model:
+    v_age_names <- n_age_init:(n_age_init + n_t - 1) # vector with age names
+    v_n <- c("H", "S1", "S2", "D")  # vector with the 4 health states of the model:
                                     # Healthy (H), Sick (S1), Sicker (S2), Dead (D)
-    n.states <- length(v.n)         # number of health states 
-    v.s.init <- c(H = 1, S1 = 0, S2 = 0, D = 0) # initial state vector
+    n_states <- length(v_n)         # number of health states 
+    v_s_init <- c(H = 1, S1 = 0, S2 = 0, D = 0) # initial state vector
     #### Create list with all parameters ####
-    l.params.all <- list(
-      n.age.init  = n.age.init, 
-      n.t         = n.t       , 
-      v.age.names = v.age.names,
-      v.n = v.n,
-      n.states = n.states,
-      v.s.init = c(H = 1, S1 = 0, S2 = 0, D = 0),
-      v.r.mort_by_age = v.r.mort_by_age
+    l_params_all <- list(
+      n_age_init  = n_age_init, 
+      n_t         = n_t       , 
+      v_age_names = v_age_names,
+      v_n = v_n,
+      n_states = n_states,
+      v_s_init = c(H = 1, S1 = 0, S2 = 0, D = 0),
+      v_r_mort_by_age = v_r_mort_by_age
     )
-    return(l.params.all)
+    return(l_params_all)
   }
   )
   
-  l.params.all <- c(l.params.all, 
-                    df.params.init) # Add initial set of parameters
+  l_params_all <- c(l_params_all, 
+                    df_params_init) # Add initial set of parameters
 }
--- R/02_simulation_model_functions.R
+++ R/02_simulation_model_functions.R
@@ -1,67 +1,67 @@
-f.decision_model <- function(l.params.all, verbose = FALSE){ # User defined
+decision_model <- function(l_params_all, verbose = FALSE){ # User defined
   ### Definition:
   ##   Decision model implementation function
   ### Arguments:  
-  ##   l.params.all: List with all parameters of decision model
+  ##   l_params_all: List with all parameters of decision model
   ##   verbose: Logical variable to indicate print out of messages
   ### Returns:
-  ##   a.P: Transition probability array
-  ##   m.M: Matrix cohort trace
+  ##   a_P: Transition probability array
+  ##   m_M: Matrix cohort trace
   ##
-  with(as.list(l.params.all), {
+  with(as.list(l_params_all), {
     #### Age-specific transition probabilities ####
     # Mortality for healthy individuals
-    p.HDage  <- 1 - exp(-v.r.mort_by_age[(n.age.init + 1) + 0:(n.t - 1)])        
+    p_HDage  <- 1 - exp(-v_r_mort_by_age[(n_age_init + 1) + 0:(n_t - 1)])        
     # Mortality for sick individuals
-    p.S1Dage <- 1 - exp(-v.r.mort_by_age[(n.age.init + 1) + 0:(n.t - 1)] * hr.S1)
+    p_S1Dage <- 1 - exp(-v_r_mort_by_age[(n_age_init + 1) + 0:(n_t - 1)] * hr_S1)
     # Mortality for sicker individuals
-    p.S2Dage <- 1 - exp(-v.r.mort_by_age[(n.age.init + 1) + 0:(n.t - 1)] * hr.S2)
+    p_S2Dage <- 1 - exp(-v_r_mort_by_age[(n_age_init + 1) + 0:(n_t - 1)] * hr_S2)
     
     #### Create age-specific transition probability matrices in an array ####
     # Initialize array
-    a.P <- array(0, dim = c(n.states, n.states, n.t),
-                 dimnames = list(v.n, v.n, 0:(n.t-1)))
+    a_P <- array(0, dim = c(n_states, n_states, n_t),
+                 dimnames = list(v_n, v_n, 0:(n_t-1)))
     # Fill in array
     # From H
-    a.P["H", "H", ]  <- (1-p.HDage) * (1 - p.HS1)
-    a.P["H", "S1", ] <- (1-p.HDage) * p.HS1
-    a.P["H", "D", ]  <- p.HDage
+    a_P["H", "H", ]  <- (1-p_HDage) * (1 - p_HS1)
+    a_P["H", "S1", ] <- (1-p_HDage) * p_HS1
+    a_P["H", "D", ]  <- p_HDage
     # From S1
-    a.P["S1", "H", ]  <- (1-p.S1Dage) * p.S1H
-    a.P["S1", "S1", ] <- (1-p.S1Dage) * (1 - (p.S1S2 + p.S1H))
-    a.P["S1", "S2", ] <- (1-p.S1Dage) * p.S1S2
-    a.P["S1", "D", ]  <- p.S1Dage
+    a_P["S1", "H", ]  <- (1-p_S1Dage) * p_S1H
+    a_P["S1", "S1", ] <- (1-p_S1Dage) * (1 - (p_S1S2 + p_S1H))
+    a_P["S1", "S2", ] <- (1-p_S1Dage) * p_S1S2
+    a_P["S1", "D", ]  <- p_S1Dage
     # From S2
-    a.P["S2", "S2", ] <- 1 - p.S2Dage
-    a.P["S2", "D", ]  <- p.S2Dage
+    a_P["S2", "S2", ] <- 1 - p_S2Dage
+    a_P["S2", "D", ]  <- p_S2Dage
     # From D
-    a.P["D", "D", ] <- 1
+    a_P["D", "D", ] <- 1
     
     # Check if transition probabilities are valid (i.e., in [0, 1])
-    m.indices.notvalid <- arrayInd(which(a.P < 0 | a.P > 1), 
-                                   dim(a.P))
+    m_indices_notvalid <- arrayInd(which(a_P < 0 | a_P > 1), 
+                                   dim(a_P))
     try(
-    if(dim(m.indices.notvalid)[1] != 0){
-      v.rows.notval   <- rownames(a.P)[m.indices.notvalid[, 1]]
-      v.cols.notval   <- colnames(a.P)[m.indices.notvalid[, 2]]
-      v.cycles.notval <- dimnames(a.P)[[3]][m.indices.notvalid[, 3]]
+    if(dim(m_indices_notvalid)[1] != 0){
+      v_rows_notval   <- rownames(a_P)[m_indices_notvalid[, 1]]
+      v_cols_notval   <- colnames(a_P)[m_indices_notvalid[, 2]]
+      v_cycles_notval <- dimnames(a_P)[[3]][m_indices_notvalid[, 3]]
       
-      df.notvalid <- data.frame(`Transition probabilities not valid:` = 
-                                  matrix(paste0(paste(v.rows.notval, v.cols.notval, sep = "->"),
+      df_notvalid <- data.frame(`Transition probabilities not valid:` = 
+                                  matrix(paste0(paste(v_rows_notval, v_cols_notval, sep = "->"),
                                                 "; at cycle ",
-                                                v.cycles.notval), ncol = 1), 
+                                                v_cycles_notval), ncol = 1), 
                                 check.names = FALSE)
       if(verbose){
         message("Not valid transition probabilities")
-        # print(df.notvalid)
-        stop(print(df.notvalid), call. = FALSE)
+        # print(df_notvalid)
+        stop(print(df_notvalid), call. = FALSE)
       } #else stop()
     }
     )
     
     # Check if transition probability array is valid
-    valid <- apply(a.P, 3, function(x) all.equal(sum(rowSums(x)), n.states))
-    if (!isTRUE(all.equal(as.numeric(sum(valid)), as.numeric(n.t)))) {
+    valid <- apply(a_P, 3, function(x) all.equal(sum(rowSums(x)), n_states))
+    if (!isTRUE(all_equal(as.numeric(sum(valid)), as.numeric(n_t)))) {
       if(verbose){
         stop("This is not a valid transition Matrix")
       } #else stop()
@@ -69,18 +69,18 @@ f.decision_model <- function(l.params.all, verbose = FALSE){ # User defined
     
     #### Compute cohort trace matrix and transition array for age-dependent STM ####
     # Initialize cohort trace matrix
-    m.M <- matrix(0, 
-                  nrow = (n.t + 1), ncol = n.states, 
-                  dimnames = list(0:n.t, v.n))
+    m_M <- matrix(0, 
+                  nrow = (n_t + 1), ncol = n_states, 
+                  dimnames = list(0:n_t, v_n))
     # Set first row of m.M with the initial state vector
-    m.M[1, ] <- v.s.init
+    m_M[1, ] <- v_s_init
     
     # Iterate STM over time
-    for(t in 1:n.t){
-      m.M[t + 1, ] <- m.M[t, ] %*% a.P[, , t]
+    for(t in 1:n_t){
+      m_M[t + 1, ] <- m_M[t, ] %*% a_P[, , t]
     }
-    return(list(a.P = a.P,
-                m.M = m.M))
+    return(list(a_P = a_P,
+                m_M = m_M))
   }
   )
 }
