34c44902816efda49be3b3ed686ba7e173a3631d
Fernando Alarid-Escudero <alari006@umn.edu>
2019-07-15 18:19:16 -0500

    Update validation functions

--- R/04_validation_functions.R
+++ R/04_validation_functions.R
@@ -1,17 +1,14 @@
-#-------------------------------------------------# 
-#### Function to summarize posterior output  ######
-#-------------------------------------------------# 
 #' Summarize posterior output
 #'
-#' \code{data_summary} is used to to calculate the mean, standard deviation and 95% Credible Interval.
-#' @param data Data frame
-#' @param varname Name of a column containing the variable
-#' @param groupnames Vector of column names to be used as grouping variables
-#' @return A data frame containing the posterior output.
-#' 
-
+#' \code{data_summary} is used to to calculate the mean, standard deviation and 
+#' 95% credible interval.
+#' @param data Data frame.
+#' @param varname Name of a column containing the variable.
+#' @param groupnames Vector of column names to be used as grouping variables.
+#' @return 
+#' A data frame containing the posterior output.
+#' @export
 data_summary <- function(data, varname, groupnames){
-  require(plyr)
   summary_func <- function(x, col){
     c(mean = mean(x[[col]], na.rm = TRUE),
       median = quantile(x[[col]], probs = 0.5, names = FALSE),
@@ -19,7 +16,7 @@ data_summary <- function(data, varname, groupnames){
       lb = quantile(x[[col]], probs = 0.025, names = FALSE),
       ub = quantile(x[[col]], probs = 0.975, names = FALSE))
   }
-  data_sum <- ddply(data, groupnames, .fun = summary_func, 
+  data_sum <- plyr::ddply(data, groupnames, .fun = summary_func, 
                     varname)
   data_sum <- plyr::rename(data_sum, c("mean" = varname))
   return(data_sum)
