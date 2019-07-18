e750778ebf050b4316430a953996a9db5dfb7cca
zoekao <kaoxx085@umn.edu>
2019-07-18 13:17:08 -0500

    add more detailed comments in tests

--- tests/test_05a_deterministic_analysis_function.R
+++ tests/test_05a_deterministic_analysis_function.R
@@ -14,13 +14,17 @@ source(paste0(workdir, "/R/02_simulation_model_functions.R"))
 source(paste0(workdir, "/R/05a_deterministic_analysis_functions.R"))
 
 
+#### Unit tests start ####
 test_that("checking output", {
+  ## generate the output from calculate_ce_out
   ce_table <- calculate_ce_out(l_params_all)
   
-  expect_true(all(colnames(ce_table) == c("Strategy", "Cost", "Effect", "NMB")))
-  expect_true(is.factor(ce_table$Strategy))
+  # check the expected column names
+  expect_identical(colnames(ce_table), c("Strategy", "Cost", "Effect", "NMB"))
+  # check whether the strategies are either factor or character
+  expect_true(is.factor(ce_table$Strategy) | is.character(ce_table$Strategy))
+  # check whether Cost, Effect, and NMB columns are numeric
   expect_true(all(is.numeric(ce_table$Cost), is.numeric(ce_table$Effect), is.numeric(ce_table$NMB)))
-  expect_true(all(ce_table$Strategy == l_params_all$v_names_str))
+  # check whether the strategies are identical to what we expect
+  expect_identical(as.character(ce_table$Strategy), l_params_all$v_names_str)
 })
-
-
